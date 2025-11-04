2ccd5f8b29f3c6692dc7fc5f388288023ded0f2e
Victor Oldensand <victormag24@gmail.com>
2025-11-04 10:41:46 -0800

    ✨ create data types for workflow graph

--- lelamp/service/workflows/workflow_service.py
+++ lelamp/service/workflows/workflow_service.py
@@ -1,19 +1,22 @@
 
 import json
 
+from lelamp.service.workflows.workflow import Workflow
+
 
 class WorkflowService:
     def __init__(self):
         self.active_workflow = None
         self.state = None
+        self.workflow_graph = None
         
-
-    def load_workflow(self, workflow_name: str):
+    def generate_workflow(self, workflow_name: str):
         with open(f"lelamp/workflows/{workflow_name}.json", "r") as f:
             self.workflows[workflow_name] = json.load(f)
+            self.workflow_graph = Workflow.from_json(self.workflows[workflow_name])
             
-    def generate_workflow_graph(self, workflow_name: str):
-        return self.workflows[workflow_name]
-
     def get_next_step(self):
-        return self.workflows[workflow_name]["next_step"]
+        if self.workflow_graph is None:
+            return "Error: No workflow graph found. Please select a workflow first."
+        current_node = self.workflow_graph.get_current_node()
+        return 
